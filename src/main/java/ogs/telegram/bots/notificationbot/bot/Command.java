1ac11420596450557282a76fdcde831b79ac0453
goshan <goshan@asus.home>
2018-04-02 22:52:52 +0300

    Added Command class + pom dependency.

---
+++ src/main/java/ogs/telegram/bots/notificationbot/bot/Command.java
@@ -0,0 +1,18 @@
+package ogs.telegram.bots.notificationbot.bot;
+
+import org.telegram.telegrambots.api.objects.Chat;
+import org.telegram.telegrambots.api.objects.User;
+import org.telegram.telegrambots.bots.AbsSender;
+import org.telegram.telegrambots.bots.commandbot.commands.BotCommand;
+
+public class Command extends BotCommand {
+
+    public Command(String commandIdentifier, String description) {
+        super(commandIdentifier, description);
+    }
+
+    @Override
+    public void execute(AbsSender absSender, User user, Chat chat, String[] strings) {
+
+    }
+}
